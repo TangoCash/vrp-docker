59fd7fe0fe3825c77f5b7745f25f43af4701f239
TangoCash <eric@loxat.de>
2024-09-10 20:58:08 +0200

    Update entrypoint.sh

--- photon/entrypoint.sh
+++ photon/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Start import"
-java -jar ./photon.jar -nominatim-import -host ${NOMINATIM_HOST} -port 5432 -database nominatim -user nominatim -password ${NOMINATIM_PASSWORD}
+java -jar ./photon.jar -cors-any -nominatim-import -host ${NOMINATIM_HOST} -port 5432 -database nominatim -user nominatim -password ${NOMINATIM_PASSWORD}
 echo "Start photon"
 java -jar ./photon.jar -cors-any $@
