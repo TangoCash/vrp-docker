e350f78a6ff881241491c4164c42f98e8abfed1f
TangoCash <eric@loxat.de>
2024-09-10 21:05:41 +0200

    Update entrypoint.sh

--- photon/entrypoint.sh
+++ photon/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-echo "Start import"
-java -jar ./photon.jar -cors-any -nominatim-import -host ${NOMINATIM_HOST} -port 5432 -database nominatim -user nominatim -password ${NOMINATIM_PASSWORD}
-echo "Start photon"
-java -jar ./photon.jar -cors-any $@
+
+# import elasticsearch index
+if [ ! -d "./photon_data/elasticsearch" ]; then
+    echo "Importing search index"
+    java -jar ./photon.jar -cors-any -nominatim-import -host ${NOMINATIM_HOST} -port 5432 -database nominatim -user nominatim -password ${NOMINATIM_PASSWORD}
+fi
+
+# Start photon if elastic index exists
+
+if [ -d "./photon_data/elasticsearch" ]; then
+    echo "Start photon"
+    java -jar ./photon.jar -cors-any $@
+else
+    echo "Could not start photon, the search index could not be found"
+fi
