1bd51b543ae52499bce9176ee83795cc12d3ba2a
TangoCash <eric@loxat.de>
2024-09-10 18:58:34 +0200

    add photon to stack

--- photon/entrypoint.sh
+++ photon/entrypoint.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
+echo "Start photon"
+java -jar ./photon.jar -cors-any -nominatim-import -host ${NOMINATIM_HOST} -port 5432 -database nominatim -user nominatim -password ${NOMINATIM_PASSWORD} $@
 
-USER_AGENT="docker: tangocash/photon-geocoder"
-
-# Download elasticsearch index
-if [ ! -d "./photon_data/elasticsearch" ]; then
-    echo "Downloading search index"
-
-    if [ "$EXTRACT_COUNTRY" == "all" ]; then
-        # Download all countries
-        wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | pbzip2 -cd | tar x
-    else
-        # Download specific country
-        wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/extracts/by-country-code/${EXTRACT_COUNTRY}/photon-db-${EXTRACT_COUNTRY}-latest.tar.bz2 | pbzip2 -cd | tar x
-    fi
-fi
-
-# Start photon if elastic index exists
-
-if [ -d "./photon_data/elasticsearch" ]; then
-    echo "Start photon"
-    java -jar ./photon.jar $@
-else
-    echo "Could not start photon, the search index could not be found"
-fi
